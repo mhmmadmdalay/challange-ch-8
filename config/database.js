09a523f3088bd4d4c64ee4d38023e841ea9f8f94
mhmmadmdalay <mhmmadmalay@gmail.com>
2022-06-12 01:09:05 +0700

    push challange 08

--- config/database.js
+++ config/database.js
@@ -1,34 +1,61 @@
+const secretData = {
+  JWT_SIGNATURE_KEY : 'Rahasia',
+  DB_USER: 'svhylpxwcpukyp',
+  DB_PASSWORD: '79b113ea40d37adf027b3836e5c9ccd39820f2f4252490e64126ba44aa59b6d',
+  DB_NAME: 'd3dr0bnkve9rib',
+  DB_HOST: 'ec2-3-226-163-72.compute-1.amazonaws.com',
+  DB_PORT: '5432'
+}
+
 const {
   DB_USER = "",
   DB_PASSWORD = "",
   DB_NAME = "bcr",
   DB_HOST = "127.0.0.1",
   DB_PORT = "5432",
-} = process.env;
+} = secretData;
 
 module.exports = {
   development: {
     username: DB_USER,
     password: DB_PASSWORD,
-    database: `${DB_NAME}_development`,
+    database: `${DB_NAME}`,
     host: DB_HOST,
     port: DB_PORT,
-    dialect: "postgres"
+    dialect: "postgres", 
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   },
   test: {
     username: DB_USER,
     password: DB_PASSWORD,
-    database: `${DB_NAME}_test`,
+    database: `${DB_NAME}`,
     host: DB_HOST,
     port: DB_PORT,
-    dialect: "postgres"
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   },
   production: {
     username: DB_USER,
     password: DB_PASSWORD,
-    database: `${DB_NAME}_production`,
+    database: `${DB_NAME}`,
     host: DB_HOST,
     port: DB_PORT,
-    dialect: "postgres"
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   }
 }
