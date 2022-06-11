79274d5131315791532d2236ffcd28f2bd161d50
mhmmadmdalay <mhmmadmalay@gmail.com>
2022-06-12 01:35:14 +0700

    push challange 08

--- config/database.js
+++ config/database.js
@@ -1,11 +1,3 @@
-const secretData = {
-  JWT_SIGNATURE_KEY : 'Rahasia',
-  DB_USER: 'svhylpxwcpukyp',
-  DB_PASSWORD: '79b113ea40d37adf027b3836e5c9ccd39820f2f4252490e64126ba44aa59b6d',
-  DB_NAME: 'd3dr0bnkve9rib',
-  DB_HOST: 'ec2-3-226-163-72.compute-1.amazonaws.com',
-  DB_PORT: '5432'
-}
 
 const {
   DB_USER = "",
@@ -13,7 +5,7 @@ const {
   DB_NAME = "bcr",
   DB_HOST = "127.0.0.1",
   DB_PORT = "5432",
-} = secretData;
+} = process.env;
 
 module.exports = {
   development: {
